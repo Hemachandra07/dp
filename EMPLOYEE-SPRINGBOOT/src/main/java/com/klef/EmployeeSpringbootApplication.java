0c05ea3c582c7652b059957b6ecb07d46e4ee5b4
HemaChandra <hemachandrareddy07@gmail.com>
2025-09-16 01:03:20 +0530

    updated sprinboot

--- EMPLOYEE-SPRINGBOOT/src/main/java/com/klef/EmployeeSpringbootApplication.java
+++ EMPLOYEE-SPRINGBOOT/src/main/java/com/klef/EmployeeSpringbootApplication.java
@@ -2,12 +2,14 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeSpringbootApplication {
+public class EmployeeSpringbootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeSpringbootApplication.class, args);
+		System.out.println("Hii Devops");
 	}
 
 }

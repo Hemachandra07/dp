3605b2ba4192c180388dea38e9ef4bbb1175c4ca
HemaChandra <hemachandrareddy07@gmail.com>
2025-10-15 16:23:46 +0530

    Adding Project

--- EMPLOYEE-REACT/src/components/config.js
+++ EMPLOYEE-REACT/src/components/config.js
@@ -1,5 +1,5 @@
 const config = {
-  url: "http://localhost:8085/springbootemployee"   // matches your Spring Boot server.port
+  url: "http://localhost:2030/springbootemployee"   // matches your Spring Boot server.port
 };
 
 export default config;
